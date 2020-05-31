597a650d312a87d9b0d0c98ba864e3123bb31e95
ComradeCynical <msaurondon@gmail.com>
2020-05-31 11:30:31 +0200

    a

--- scripts/OrionSingleBar.js
+++ scripts/OrionSingleBar.js
@@ -7,3 +7,43 @@
 
 let highestHigh = 0.0;
 let lowestLow = 0.0;
+let barsBack = 11; // 10 days not including current;
+
+export function OrionSingleBar(jsonObj){
+  let transactionDate = today;
+  let lastTradeDate = jsonObj[Object.keys(jsonObj)[0]];
+
+  try{
+    for(var x in jsonObj){
+      transactionDate = Object.keys(jsonObj)[barsBack];
+      if(barsBack <= 1){
+        fetchBoundaries(jsonObj[transactionDate]);
+        barsBack--;
+      }
+      else {
+        break;
+      }
+    }
+  }
+  catch(err){
+    console.log("createSMA: "+err);
+  }
+
+
+
+}
+
+function fetchBoundaries(jsonData){
+  let high = jsonData["2. high"];
+  let low = jsonData["3. low"];
+  higher(high);
+  lower(low);
+}
+
+function higher(high){
+  highestHigh = highestHigh > high ? highestHigh : high;
+}
+
+function lower(low){
+  lowestLow = lowestLow < low ? lowestLow : low;
+}
